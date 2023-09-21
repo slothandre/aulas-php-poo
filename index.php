d497c03377ac92bef742d787d1696f47077210d1
André Marques <andre.m.d.abreu@gmail.com>
2023-09-21 11:14:03 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo</title>
+</head>
+<body>
+    <header>
+        <h1>PHP com POO</h1>
+        <hr>
+    </header>
+</body>
+</html>
