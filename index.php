0cc7542d3ff9a6a3435fb2195b2a9eb292329449
André Marques <andre.m.d.abreu@gmail.com>
2023-09-22 09:13:53 -0300

    Atribuição de valores usando método constructor

--- index.php
+++ index.php
@@ -13,50 +13,15 @@
     <main>
         <h2>Assuntos abordados:</h2>
         <ul>
-            <li>Acesso direto às propriedades</li>
-            <li>Atribuição e leitura de dados</li>
-            <li>Chamada de métodos</li>
-            <li>Uso do <code>$this</code> para acessar recursos dentro da própria classe</li>
+            <li>Método construtor com atribuição obrigatória de valores aos parâmetros/propriedades do objeto</li>
+            <li>Uso do <code>$this</code> para acesso às propriedades dentro do objeto</li>
         </ul>
 
         <?php
             require_once "./src/Cliente.php";
-            
-            $clienteA = new Cliente;
-            $clienteB = new Cliente;
-
-            // Acesso e atribuição
-            $clienteA->nome = "André";
-            $clienteB->nome = "Tiuliel";
-
-            $clienteA->telefones = ["11-2135-0300", "11-93011-9189"];
-
-            $clienteA->email = "andre.m.d.abreu@gmail.com";
-            $clienteA->senha = password_hash("123abc", PASSWORD_DEFAULT);
+            $clienteA = new Cliente("André", "xpto@ok.com");
+            $clienteB = new Cliente("Ozzy", "ozzy@msn.com");
         ?>
-        <hr>
-
-        <h2>Dados dos objetos (acesso e leitura)</h2>
-        <h3><?=$clienteA->nome?></h3>
-        <p>E-mail: <?=$clienteA->email?></p>
-        <p>
-            Telefones (acesso um por um):
-            <?=$clienteA->telefones[0]?>, <?=$clienteA->telefones[1]?>
-        </p>
-        <p>
-            Telefones (convertendo para string):
-            <?=implode(", ", $clienteA->telefones)?>
-        </p>
-        <p>Telefones (usando loop):</p>
-        <ul>
-            <?php foreach($clienteA->telefones as $telefone) { ?>
-                <li><?=$telefone?></li>
-            <?php } ?>
-        </ul>
-
-        <h2>Acessando os dados através de um método</h2>
-        <?=$clienteA->exibirDados()?>
-        <?=$clienteB->exibirDados()?>
         <pre><?=var_dump($clienteA, $clienteB)?></pre>
     </main>
 </body>
--- src/Cliente.php
+++ src/Cliente.php
@@ -7,12 +7,18 @@ class Cliente {
     public string $senha;
     public array $telefones;
 
-    // Métodos (ou comportamentos)
-    public function exibirDados():void {
-        echo "<section>";
-        echo "<h2>$this->nome</h2>";
-        echo "<p>$this->email</p>";
-        echo "</section>";
+    /* Método construtor
+    Permite configurar a forma de inicialização
+    do objeto, exigindo a atribuição de dados
+    no momento de criar o objeto/instância.
+    O construtor é sempre chamado de forma
+    automática assim que o objeto é criado */
+    public function __construct(string $nome, string $email) {
+        /* Atribuindo os valores dos parâmetros
+        às propriedades. Usamos o $this para acessar
+        as propriedades desta Classe. */
+        $this->nome = $nome;
+        $this->email = $email;    
     }
 
 }
