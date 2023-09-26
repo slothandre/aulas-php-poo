fb9acfa1721f24baf8000f8365d949b8c17faf40
André Marques <andre.m.d.abreu@gmail.com>
2023-09-26 09:04:51 -0300

    Create PessoaFisica.php

---
+++ src/PessoaFisica.php
@@ -0,0 +1,27 @@
+<?php
+    class PessoaFisica {
+        private int $idade;
+        private string $cpf;
+
+        public function getIdade(): int
+        {
+                return $this->idade;
+        }
+        public function setIdade(int $idade): self
+        {
+                $this->idade = $idade;
+
+                return $this;
+        }
+        
+        public function getCpf(): string
+        {
+                return $this->cpf;
+        }
+        public function setCpf(string $cpf): self
+        {
+                $this->cpf = $cpf;
+
+                return $this;
+        }
+    }
