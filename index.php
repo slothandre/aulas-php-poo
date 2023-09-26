83e827c95e1d3d5647b1551b8d65b240ae209780
André Marques <andre.m.d.abreu@gmail.com>
2023-09-26 08:57:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,39 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 4</title>
+    <title>Exemplo 5</title>
 </head>
 <body>
     <header>
-        <h1>PHP com POO - Exemplo 4</h1>
+        <h1>PHP com POO - Exemplo 5</h1>
         <hr>
     </header>
     <main>
         <h2>Assuntos abordados:</h2>
         <ul>
-            <li>Encapsulamento</li>
-            <li>Modificadores de visibilidade (public, private e protected)</li>
+            <li>Herança (Super Classe e Sib Classe)</li>
         </ul>
-
-        <?php
-            require_once "./src/Cliente.php";
-            $clienteA = new Cliente;
-
-            /* O código abaixo dará erro pois a propriedade
-            é privada! */
-            // $clienteA->nome = "André";
-
-            /* Usando o setter para atribuir um valor
-            à propriedade privada "nome" */
-            $clienteA->setNome("André");
-            $clienteA->setEmail("xpto@ok.com");
-            $clienteA->setSenha("senhaSegura123");
-        ?>
-        <!-- Usando o getter para ler/acessar dados privados -->
-        <p>Nome: <?=$clienteA->getNome()?></p>
-        <p>E-mail: <?=$clienteA->getEmail()?></p>
-        <p>Senha: <?=$clienteA->getSenha()?></p>
-        <pre><?=var_dump($clienteA)?></pre>
     </main>
 </body>
 </html>
