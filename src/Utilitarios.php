83518bd26e26265e799bdfb06f109c371c35d4fc
André Marques <andre.m.d.abreu@gmail.com>
2023-09-28 11:35:17 -0300

    Create Utilitarios.php

---
+++ src/Utilitarios.php
@@ -0,0 +1,12 @@
+<?php
+    class Utilitarios {
+        // Propriedade estática
+        public static string $dataAtual;
+
+        // Método estático
+        public static function obterData(){
+            /* self::propriedade
+            Permite o acesso à propriedade estática */
+            self::$dataAtual = date("d/m/Y");
+        }
+    }
