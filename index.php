0161c535907632e194f387e2872ae703861d63e0
André Marques <andre.m.d.abreu@gmail.com>
2023-09-28 11:32:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,50 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 6</title>
+    <title>Exemplo 8</title>
 </head>
 <body>
     <header>
-        <h1>PHP com POO - Exemplo 6</h1>
+        <h1>PHP com POO - Exemplo 8</h1>
         <hr>
     </header>
     <main>
         <h2>Assuntos abordados:</h2>
         <ul>
-            <li>Polimorfismo</li>
-            <li>Sobreposição de métodos</li>
-            <li>Uso do <code>parent</code> para acessar superclasse</li>
+            <li>Propriedades e métodos estáticos</li>
+            <li>Acesso direto sem objetos</li>
         </ul>
-
-        <?php
-            require_once "./src/PessoaFisica.php";
-            require_once "./src/PessoaJuridica.php";
-            $clientePF = new PessoaFisica;
-
-            // Colocar um nome no Cliente PF
-            $clientePF->setNome("Fulano");
-            $clientePF->setEmail("fulano@gmail.com");
-            $clientePF->setIdade(20);
-            $clientePF->setCpf("123.456.789-00");
-
-            // Exercício
-            $clientePJ = new PessoaJuridica;
-            $clientePJ->setNome("Beltrano S/A");
-            $clientePJ->setEmail("blablabla@gmail.com");
-            $clientePJ->setAnoFundacao(2000);
-            $clientePJ->setCnpj("32.088.0001/000.41");
-            $clientePJ->setNomeFantasia("Bla Bla Bla Informática");
-        ?>
-        <hr>
-        <h2>Saída de Dados</h2>
-        <section>
-            <!-- Saída do cliente PessoaFísica -->
-            <?=$clientePF->exibirDados()?>
-        </section>
-        <section>
-            <!-- Saída do cliente PessoaJuridica -->
-            <?=$clientePJ->exibirDados()?>
-        </section>
     </main>
 </body>
 </html>
