48786b42f23561a3ac41178fdac3eca5a4d7f1a3
André Marques <andre.m.d.abreu@gmail.com>
2023-09-28 11:45:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,19 @@
         <ul>
             <li>Propriedades e métodos estáticos</li>
             <li>Acesso direto sem objetos</li>
+            <li>Uso do <code>self</code> para acessar (dentro da classe) aos recursos estáticos</li>
         </ul>
+
+        <?php
+            require_once "./src/PessoaFisica.php";
+            $cliente1 = new PessoaFisica;
+            $cliente1->setNome("Astrogildo");
+            $cliente1->setIdade(75);
+
+            require_once "./src/Utilitarios.php";
+            Utilitarios::obterData();
+        ?>
+        <h2>Atendimentos do dia: <?=Utilitarios::$dataAtual?></h2>
     </main>
 </body>
 </html>
