3786d8376ccd5277532b0d41ffac12087b9e5d37
André Marques <andre.m.d.abreu@gmail.com>
2023-09-28 10:42:04 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,20 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exemplo 5</title>
+    <title>Exemplo 6</title>
 </head>
 <body>
     <header>
-        <h1>PHP com POO - Exemplo 5</h1>
+        <h1>PHP com POO - Exemplo 6</h1>
         <hr>
     </header>
     <main>
         <h2>Assuntos abordados:</h2>
         <ul>
-            <li>Herança (Super Classe e Sub Classe)</li>
-            <li>Métodos/propriedades protegidos (acessíveis apenas pelas classes em que foram definidos e pelas subclasses)</li>
-            <li>Classe Abstrata: não permite instâncias/objetos, serve apenas de modelo para subclasses.</li>
-            <li>Classe Final: não permite herança, ou seja, não aceita a criação de subclasses.</li>
+
         </ul>
 
         <?php
@@ -38,24 +35,7 @@
             $clientePJ->setCnpj("32.088.0001/000.41");
             $clientePJ->setNomeFantasia("Bla Bla Bla Informática");
         ?>
-        <pre><?=var_dump($clientePF)?></pre>
-        <pre><?=var_dump($clientePJ)?></pre>
         <hr>
-        <?php
-            require_once "./src/MEI.php";
-            $clienteMei = new MEI;
-            $clienteMei->setNome("Marina Tanaka");
-            $clienteMei->setAnoFundacao(2023);
-            $clienteMei->setNomeFantasia("Tanaka Empreendimentos");
-            $clienteMei->setAreaDeAtuacao("Ervas medicinais");
-        ?>
-        <pre><?=var_dump($clienteMei)?></pre>
-        
-        <?php
-            require_once "./src/Cliente.php";
-            $clienteGenerico = new Cliente;
-        ?>
-        <pre><?=var_dump($clienteGenerico)?></pre>
     </main>
 </body>
 </html>
